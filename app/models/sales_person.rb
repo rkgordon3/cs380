70849d6d4c74c5f475aa97eef7c458ef16557837
Rob Gordon <rgordon@smumn.edu>
2013-03-05 18:28:49 -0600

    improved commentary

--- app/models/sales_person.rb
+++ app/models/sales_person.rb
@@ -14,11 +14,26 @@ class SalesPerson < ActiveRecord::Base
 
   #
   # All orders for which this sales person is responsible
+  #
   def orders
   	#SELECT "orders".* FROM "orders" 
   	#  INNER JOIN "customers" ON "customers"."id" = "orders"."customer_id" 
   	#  WHERE "customers"."sales_person_id" = self.id
 
   	 Order.joins(:customer).where(customers: { sales_person_id: self.id})
+
+    # The above query is functionally equivalent to 
+    # orders = []
+    # customers.each { |c|
+    #     orders << c.orders.flatten
+    # }
+    # but it is NOT equivalent performance-wise. The query above hits
+    # the database once. The second form hits the database 1+N times
+    # where N is number of customers associated with this sales person.
+    # (Note: some improvements could maybe be gained via caching, but
+    # there is no need for that given alternative above).
+    #
+    # Performance aside, the second solution just looks ugly. Stay away from 
+    # such approaches.
   end
 end
